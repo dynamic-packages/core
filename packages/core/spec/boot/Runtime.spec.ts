21d9f8953b6b423e2a003646e41818ff18f502b3
Shim Heungwoon <ajaxlab7@gmail.com>
2019-02-19 03:53:15 +0900

    [env] support for vscode debugger

--- packages/core/spec/boot/Runtime.spec.ts
+++ packages/core/spec/boot/Runtime.spec.ts
@@ -1,11 +1,14 @@
 import {expect, use} from 'chai';
+import Runtime from '../../src/boot/Runtime';
 
 describe('Runtime', function () {
 
   describe('start()', function () {
 
     it('Creates a SystemPlugin instance', function () {
-      expect(true).to.be.true;
+      const runtime = new Runtime({});
+      runtime.start();
+      expect(runtime).to.equal(runtime);
     });
 
   });
