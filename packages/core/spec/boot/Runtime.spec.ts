56cd32a00ab446edfe12d1127e85187f3287990b
Shim Heungwoon <ajaxlab7@gmail.com>
2019-02-19 02:12:31 +0900

    [env] support for unit test

---
+++ packages/core/spec/boot/Runtime.spec.ts
@@ -0,0 +1,13 @@
+import {expect, use} from 'chai';
+
+describe('Runtime', function () {
+
+  describe('start()', function () {
+
+    it('Creates a SystemPlugin instance', function () {
+      expect(true).to.be.true;
+    });
+
+  });
+
+});
