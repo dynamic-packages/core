6800df53fdce75609635690813d45258d9904672
Shim Heungwoon <ajaxlab7@gmail.com>
2019-09-26 02:37:35 +0900

    [common] enhance argsToStr, stringify

--- packages/core-common/src/array/argsToStr.ts
+++ packages/core-common/src/array/argsToStr.ts
@@ -1,9 +1,11 @@
 import { stringify } from '../object';
 
-export default function argsToStr(arg: any, quote: boolean = true): string {
+export default function argsToStr(
+  arg: any, quote: boolean = true, flat: boolean = false
+): string {
   const type = typeof arg;
   if (type === 'object') {
-    return stringify(arg);
+    return stringify(arg, flat);
   } else if (type === 'function') {
     return `<Function>(${arg.name || 'anonymous'})`;
   } else if (type === 'string') {
--- packages/core-common/src/object/stringify.ts
+++ packages/core-common/src/object/stringify.ts
@@ -4,28 +4,45 @@ const regComma = /,/g;
 const regQuote = /\"/g;
 const regProp = /\"(\w+)\":/g;
 
-function simpleJson(obj: object) {
+function json(obj: object) {
   return JSON.stringify(obj)
     .replace(regProp, '$1:')
     .replace(regQuote, '\'')
     .replace(regComma, ', ');
 }
 
-export default function stringify(arg: IKeyValue): string {
+function flatten(obj: any) {
+  const items = Object.keys(obj).map((key) => {
+    let value = obj[key];
+    const type = typeof value;
+    if (type === 'string') {
+      value = `'${value}'`;
+    } else if (type === 'object') {
+      value = value.toString();
+      if (value === '[object Object]') {
+        value = `<${obj.constructor.name}>`;
+      }
+    }
+    return key + ':' + value;
+  });
+  return '{' + items.join(', ') + '}';
+}
+
+export default function stringify(arg: IKeyValue, flat: boolean = false): string {
   const type = typeof arg;
   if (!arg || type !== 'object') {
     return arg + '';
   }
   if (type === 'object') {
     try {
-      return simpleJson(arg);
+      return flat ? flatten(arg) : json(arg);
     } catch (e) {
-      const valueStringFied = Object.keys(arg)
+      const valueStringfied = Object.keys(arg)
         .reduce((acc: IKeyValue, key) => {
           acc[key] = arg[key] + '';
           return acc;
         }, {});
-      return simpleJson(valueStringFied);
+      return flat ? flatten(valueStringfied) : json(valueStringfied);
     }
   }
   const argStr = arg.toString();
