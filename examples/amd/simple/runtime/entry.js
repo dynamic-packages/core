55ca4d63dcaeb89bbf999dfda12b7e3d005ff091
Shim Heungwoon <ajaxlab7@gmail.com>
2019-10-03 01:26:55 +0900

    [example/amd] fix entry.js

--- examples/amd/simple/runtime/entry.js
+++ examples/amd/simple/runtime/entry.js
@@ -1,6 +1,6 @@
-if (window.Promise && window.Promise.prototype.finally) {
+if (window.Promise && window.Promise.prototype && window.Promise.prototype.finally) {
   main();
 } else {
-  const polyfill = 'https://polyfill.io/v3/polyfill.js?features=es5,es6,es7';
+  const polyfill = 'https://polyfill.io/v3/polyfill.min.js?features=es5,es6,es7,es8';
   require([polyfill], main);
 }
