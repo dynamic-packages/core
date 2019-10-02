53f2c2d8f49394f3c012c160fd91025f17540aba
Shim Heungwoon <ajaxlab7@gmail.com>
2019-10-03 00:52:32 +0900

    [core-web/env] add karma test suite

---
+++ packages/core-web/spec/core-web.spec.js
@@ -0,0 +1,11 @@
+define(['dist/amd/index.min'], function (module) {
+
+  describe('some', function () {
+
+    it('does something', function () {
+      expect(1).to.equal(1);
+    });
+
+  });
+
+});
